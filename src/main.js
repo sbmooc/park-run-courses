75bc656e43489b92a692a9575d5eacc996e9a670
oli m-c <olster98@gmail.com>
2020-11-02 15:24:55 +0000

    Remove require from entrypoint file to play nicely with webpack

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import 'startbootstrap-sb-admin-2/css/sb-admin-2.css'
 import store from './store.js'
+import * as IconRetina from 'leaflet/dist/images/marker-icon-2x.png' 
+import * as IconURL from 'leaflet/dist/images/marker-icon.png'
+import * as shadowUrl from 'leaflet/dist/images/marker-shadow.png'
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+    iconRetinaUrl: IconRetina,
+    iconUrl: IconURL,
+    shadowUrl: shadowUrl 
 });
 
 
